ec27a8345858d2523ce67ed192bab86a5cf6b237
Sumitb2015 <sumitb2015@gmail.com>
2023-07-08 10:37:13 +0530

    Data Transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,60 @@
+import os
 import sys
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exceptions import CustomException
 
-def error_message_details(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info(())
-    filename=exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name : {filename}, line number : {exc_tb}, error message : {str(error)}"
-    return error_message
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
 
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_details(error_message,error_detail=error_detail)
-        
-    def __str__(self) -> str:
-        return self.error_message
+        os.makedirs(dir_path, exist_ok=True)
 
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-if __name__ == "__main__":
-    pass
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
